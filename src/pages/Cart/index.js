1e1bf1cb400d9de92c88cc2c91e2959c9aa7a8c5
Jadson Santos <jadson1197@gmail.com>
2020-12-12 01:38:31 -0300

    style: add cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,115 @@
 import React from 'react';
+import { CartList, OrderBox, MainContainer } from './styles';
+
+import productImage from '../../assets/tenis.jpg'
+
+import { BsTrash } from 'react-icons/bs'
 
 function Cart() {
   return (
-    <h1> Cart</h1>
+    <MainContainer>
+      <CartList>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+
+        <li>
+            <img src={productImage} alt={'Tenis'} />
+
+            <strong>Tenis Bacana para utilizar em corridas</strong>
+            <span>R$ 197,00</span>
+            <div className="itens-icons">
+              <BsTrash size={22} color="#000" cursor="pointer" />
+              <input type="number" name="quantity" id="quantity" min="1" max="99" defaultValue="2"/>
+            </div>
+            <span>Total: R$ 591,00 </span>
+        </li>
+      </CartList>
+
+      <OrderBox>
+        <h1>Pedido</h1>
+        <div className="subtotal">
+          <span>Subtotal</span>
+          <span>R$ 150.00</span>
+        </div>
+        <div className="promo-code">
+          <input type="text" placeholder="Inserir Cupom"/>
+        </div>
+        <div className="subtotal">
+          <span>Desconto</span>
+          <span>R$ 0,00</span>
+        </div>
+        <div className="total">
+          <span>Total</span>
+          <span>R$ 197,00</span>
+        </div>
+        <button type="button">Finalizar Venda</button>
+      </OrderBox>
+        
+    </MainContainer>
   )
 }
 
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,134 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.main`
+width: 90%;
+display: grid;
+grid-template-areas: 'card card card order';
+`
+
+export const CartList = styled.ul`
+    max-width: 80%;
+    margin: 50px 0 0 30px;
+
+    grid-area: card;
+
+    li {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        background-color: #FFF;
+        border-radius: 6px;
+        margin-bottom: 50px;
+        -webkit-box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+        -moz-box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+        box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+
+        strong {
+            font-size: 15px;
+            max-width: 90%;
+            color: #333;
+        }
+
+        span {
+            font-weight: bold;
+            margin: 10px auto;
+        }
+
+        img {
+            align-self: center;
+            max-width: 250px;
+        }
+
+        .itens-icons {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+
+
+            input {
+                width: 50px;
+                margin-left: 10px;
+            }
+        }
+
+
+        button {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            background-color: hsl(180, 8%, 28%);
+            border: 0;
+            border-radius: 4px;
+            margin-top: auto;
+            margin-bottom: 25px;
+            cursor: pointer;
+
+            transition: 0.2s;
+
+            div {
+                padding: 10px;
+                border-right: 1px solid transparent;
+                border-bottom-left-radius: 4px;
+                border-top-left-radius: 4px;
+                background-color: hsl(180, 14%, 20%);
+                margin-right: 5px;
+            }
+
+            span {
+                margin-right: 10px;
+                color: rgba(255,255,255,0.9);
+            }
+
+            &:hover {
+                opacity: 0.9;
+            }
+        }
+    }
+`
+
+export const OrderBox = styled.aside`
+    grid-area: order;
+    margin-top: 50px;
+    margin-left: 75%;
+    padding: 50px;
+    position: fixed;
+
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 6px;
+    -webkit-box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+    -moz-box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+    box-shadow: -1px 13px 20px -9px rgba(0,0,0,0.18);
+
+    h1 {
+        align-self: center;
+    }
+
+    .subtotal, .total {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+    }
+
+    .subtotal, .total, button, input {
+        margin-top: 20px;
+    }
+
+    button {
+        background-color: #27caa5;
+        color: #fff;
+        padding: 10px;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        transition: 0.2s;
+        font-weight: bold;
+        &:hover {
+            background-color: #2E806D;
+            
+        }
+    }
+`
